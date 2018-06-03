da47b9c7badc340a73fcdd7c058004daacd3f2be
yinsheng <60360329@163.com>
2018-06-03 11:46:09 +0800

    base function : change activity

---
+++ app/src/main/java/com/learning/yinsheng/parcitise_send_change/MainActivity.java
@@ -0,0 +1,38 @@
+package com.learning.yinsheng.parcitise_send_change;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXTRA_MESSAGE = "com.learning.yinsheng.parcitise_send_change.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button :
+            {
+                Intent intent = new Intent(this, showTextActivity.class);
+                EditText editText = (EditText)findViewById(R.id.editText);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        }
+    }
+}
