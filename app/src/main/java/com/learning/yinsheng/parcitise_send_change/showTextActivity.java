87cf2169c54847ad0466832620776cbf2675beed
yinsheng <60360329@163.com>
2018-06-03 11:43:08 +0800

    init

---
+++ app/src/main/java/com/learning/yinsheng/parcitise_send_change/showTextActivity.java
@@ -0,0 +1,21 @@
+package com.learning.yinsheng.parcitise_send_change;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class showTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_text);
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(msg);
+    }
+}
